df34d9c76a7d6ef490c7ff463f2cb8ab6006da67
Md. Arafat Hossain <54654401+iarafathsn@users.noreply.github.com>
2026-02-26 15:15:47 +0600

    Update rendering_native_surface.h

--- servers/rendering/rendering_native_surface.h
+++ servers/rendering/rendering_native_surface.h
@@ -33,6 +33,9 @@
 #include "core/object/class_db.h"
 #include "core/object/ref_counted.h"
 
+class RenderingContextDriver;
+class GLESContext;
+
 class RenderingNativeSurface : public RefCounted {
 	GDCLASS(RenderingNativeSurface, RefCounted);
 
@@ -41,4 +44,7 @@ class RenderingNativeSurface : public RefCounted {
 public:
 	RenderingNativeSurface();
 	~RenderingNativeSurface();
+	
+	virtual RenderingContextDriver *create_rendering_context(const String &p_driver_name) = 0;
+	virtual GLESContext *create_gles_context() { return nullptr; }
 };
